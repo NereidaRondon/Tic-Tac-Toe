7e116b30ac3d1963e97739dc5c64eb490afe6827
Nereida Rondon <Nereida.Rondon5@gmail.com>
2022-08-03 21:46:06 -0400

    Added CSS for strike through line

--- TTTscript.js
+++ TTTscript.js
@@ -4,9 +4,15 @@ let turns = 0;
 
 let currentPlayer = x;
 console.log(currentPlayer);
-let display = $('#currentPlayer').text(currentPlayer);
+let display =  $('#currentPlayer').text(currentPlayer);
+
+let winner = [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6];
+
+
+
 
 let changePlayer=()=> {
+    let display =  setTimeout(()=> $('#currentPlayer').text(currentPlayer), 500)
     console.log(`Entered changePlayer Function`);
     if(turns === 9){
     
@@ -14,18 +20,19 @@ let changePlayer=()=> {
     function handler(div) {
         div.stopPropagation();
         div.preventDefault();
-        display= $('#currentPlayer').text(`GAME OVER`);
+        setTimeout(()=> $('#currentPlayer').text(`GAME OVER`), 2000);
     }
     }
     else if(currentPlayer===x){
         currentPlayer = o;
         console.log(`changed to player-O = ${currentPlayer}`);
-        display = $('#currentPlayer').text(currentPlayer);
+       
+        display;
        
     } else {
         currentPlayer=x;
         console.log(`changed to player-X = ${currentPlayer}`);
-        display = $('#currentPlayer').text(currentPlayer);
+        display;
     };
 
     console.log(`${currentPlayer} is the current player`);
@@ -39,17 +46,15 @@ let symbolOf=(currentPlayer)=>{
     console.log('user clicked: ', event.target);
     let id = event.target.getAttribute('id');
     
-    $(document).ready(function(){
+    $(function(){
     
         $(`#${id}`).addClass(currentPlayer);
             
-        // event.target.classList.add(currentPlayer);
-        // document.getElementById(id).style.pointerEvents = 'none';
-        $(`#${id}`).click(false);
-
+        $(`#${id}`).on('click', false);
 
         console.log(`After click, ${currentPlayer} is the current player`);
         changePlayer();
     });
 };
+
 
